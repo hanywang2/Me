3070ee2e688bec09fdc8d69791bf3125b5906fca
Han Wang <hyw2@cornell.edu>
2019-06-03 20:37:55 +0800

    Resize nav bar

--- header.php
+++ header.php
@@ -35,13 +35,11 @@
 
             <div class="user-area dropdown float-right">
                 <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="user-avatar rounded-circle" src="images/admin.jpg" alt="User Avatar">
+                    <img class="user-avatar rounded-circle" src="images/admin.png" alt="User Avatar">
                 </a>
 
                 <div class="user-menu dropdown-menu">
                     <a class="nav-link" href="#"><i class="fa fa- user"></i>My Profile</a>
-
-                    <a class="nav-link" href="#"><i class="fa fa- user"></i>Notifications <span class="count">13</span></a>
 
                     <a class="nav-link" href="#"><i class="fa fa -cog"></i>Settings</a>
 
